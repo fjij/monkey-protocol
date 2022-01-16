4fbce59b84599a34c7791c500604f2bf31e8c46e
kaStinju <40683050+kaStinju@users.noreply.github.com>
2022-01-16 00:29:53 -0500

    landing background fix

--- frontend/src/Landing.tsx
+++ frontend/src/Landing.tsx
@@ -2,30 +2,34 @@ import "./landing.css";
 import ConnectWallet from "./ConnectWallet";
 import { useMonkeyRegistry } from "./contracts";
 import { useEffect, useState } from "react";
-import {Signer} from "ethers";
+import { Signer } from "ethers";
+import LandingBackground from "./assets/landing.png";
 
 export function Landing() {
-  const [signer, setSigner] = useState<Signer>()
+  const [signer, setSigner] = useState<Signer>();
   const registry = useMonkeyRegistry(signer);
-
-  useEffect(() => {
-    if (registry) {
-      registry.adopt();
-    }
-  }, [registry])
 
   return (
     <>
-      <div className="title">
-        <div>
-          <h1>Monkey Protocol</h1>
-          <h3>
-            Feed, nurse, and take your monkey on expeditions<br></br> to train
-            the coolest monkey.
-          </h3>
-        </div>
-        <div className="buttons">
-          <ConnectWallet onConnected={(signer) => {setSigner(signer)}} />
+      <div
+        className="L-background"
+        style={{ backgroundImage: `url(${LandingBackground})` }}
+      >
+        <div className="title">
+          <div>
+            <h1>Monkey Protocol</h1>
+            <h3>
+              Feed, nurse, and take your monkey on expeditions<br></br> to train
+              the coolest monkey.
+            </h3>
+          </div>
+          <div className="buttons">
+            <ConnectWallet
+              onConnected={(signer) => {
+                setSigner(signer);
+              }}
+            />
+          </div>
         </div>
       </div>
     </>
