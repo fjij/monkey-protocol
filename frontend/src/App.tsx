e50deb63190387aac98bbc28503eee377230f4c8
kaStinju <40683050+kaStinju@users.noreply.github.com>
2022-01-16 03:23:46 -0500

    expedition

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,37 +2,35 @@ import "./App.css";
 import { Landing } from "./Landing";
 import Home from "./Home";
 import { useEffect, useState } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Signer } from "ethers";
 import FullscreenMessage from "./FullscreenMessage";
+import Expedition from "./Expedition";
 
 function App() {
-  const [signer, setSigner] = useState<Signer>()
+  const [signer, setSigner] = useState<Signer>();
   const [chainId, setChainId] = useState<number>();
 
   useEffect(() => {
     signer?.getChainId().then(setChainId);
-  }, [signer])
+  }, [signer]);
 
+  //if not connect wallet landing page
   if (!signer) {
-    return <Landing onConnected={setSigner}/>;
+    return <Landing onConnected={setSigner} />;
   }
 
   if (chainId !== 31337) {
-    return <FullscreenMessage>
-      Wrong Chain Id - Switch to 31337
-    </FullscreenMessage>
+    return (
+      <FullscreenMessage>Wrong Chain Id - Switch to 31337</FullscreenMessage>
+    );
   }
 
   return (
     <Router>
       <Switch>
         <Route path="/">
-          <Home signer={signer}/>
+          <Expedition signer={signer} />
         </Route>
       </Switch>
     </Router>
